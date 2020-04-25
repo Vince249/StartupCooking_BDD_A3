cb832342847c16e5760c058ed851b860fcb57d11
Vincent Poupet <vincent.poupet@neuf.fr>
2020-04-25 09:17:13 +0200

    affichage page_CdR fini et listView produit et recette_CdR remplies

--- Projet_Startup_Cooking_BDD/Page_CdR.xaml.cs
+++ Projet_Startup_Cooking_BDD/Page_CdR.xaml.cs
@@ -25,6 +25,25 @@ namespace Projet_Startup_Cooking_BDD
         {
             InitializeComponent();
             this.id_client = id_client;
+            string query = $"Select Nom_Client from cooking.client where Identifiant = \"{this.id_client}\" ;";
+            List<List<string>> liste = Commandes_SQL.Select_Requete(query);
+            welcome_message.Content = "Bonjour " + liste[0][0];
+
+            //Ajout produit dans la listView associé
+            query = $"select Nom_Produit,Unite from cooking.produit;";
+            List<List<string>> liste_produit_nom_unite = Commandes_SQL.Select_Requete(query);
+            for (int i = 0; i < liste_produit_nom_unite.Count; i++)
+            {
+                Produits_ListView.Items.Add(new Produit { Nom_Produit = liste_produit_nom_unite[i][0], Unite = liste_produit_nom_unite[i][1] });
+            }
+
+            //Ajout recette CdR dans la listView associé
+            query = $"select Nom_Recette,Compteur from cooking.recette where Identifiant = \"{this.id_client}\";";
+            List<List<string>> liste_recette_nom_compteur = Commandes_SQL.Select_Requete(query);
+            for (int i = 0; i < liste_recette_nom_compteur.Count; i++)
+            {
+                Recettes_CdR_ListView.Items.Add(new Recette { Nom_Recette = liste_recette_nom_compteur[i][0], Compteur = liste_recette_nom_compteur[i][1] });
+            }
         }
 
         private void Client_Click(object sender, RoutedEventArgs e)
@@ -35,6 +54,42 @@ namespace Projet_Startup_Cooking_BDD
         private void Deco_Click(object sender, RoutedEventArgs e)
         {
 
+        }
+
+        private void Creer_Recette_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Supprimer_Recette_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Ajout_Produit_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Reset_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        public class Produit
+        {
+            public string Nom_Produit { get; set; }
+
+            public string Unite { get; set; }
+
+            public string Quantite { get; set; }
+        }
+
+        public class Recette
+        {
+            public string Nom_Recette { get; set; }
+
+            public string Compteur { get; set; }
         }
     }
 }
