299adda4dfad118d40b9e5821dc4bd3de044fd1e
RemiEC <guillonbonyremi@gmail.com>
2020-04-28 23:40:05 +0200

    Fini page code Page top, reste à retirer le bouton et la textbox

--- Projet_Startup_Cooking_BDD/Page_Top.xaml.cs
+++ Projet_Startup_Cooking_BDD/Page_Top.xaml.cs
@@ -47,17 +47,64 @@ namespace Projet_Startup_Cooking_BDD
                 Liste_Recette_Or.Items.Add(new Recette_Or { Nom = Nom, Type = Type, Compteur = Qte });
             }
 
+            //CdR Semaine
+            int jour = (int)DateTime.Today.DayOfWeek; //jour de la semaine
+            double decalage = 1 - jour; //on veut le decalage à lundi
+            System.DateTime datelimite_system = DateTime.Today.AddDays(decalage);
+            string datelimite = $"{datelimite_system.Day}/{datelimite_system.Month}/{datelimite_system.Year}"; // on a notre date limite
+            string date = $"{DateTime.Now.Day}/{DateTime.Now.Month}/{DateTime.Now.Year}";
+            // on veut sélectionner toutes les commandes faites sur cette période
+            query = $"select  recette.Identifiant " +
+                $"from (commande natural join composition_commande as T)  join recette " +
+                $"on recette.Nom_Recette=T.Nom_Recette " +
+                $"where Date between \"{datelimite}\" and \"{date}\" " +
+                $"group by recette.Identifiant " +
+                $"order by sum(Quantite_Recette) desc " +
+                $"limit 1;";
+            List<List<string>> Liste_CdR_Semaine = Commandes_SQL.Select_Requete(query);
+            CdR_Semaine.Content = Liste_CdR_Semaine[0][0];
+
+            //Top commande Semaine
+            query = $"select recette.Nom_Recette, Type, recette.Identifiant, sum(Quantite_Recette), Compteur " +
+                $"from (commande natural join composition_commande as T)  join recette " +
+                $"on recette.Nom_Recette=T.Nom_Recette " +
+                $"where Date between \"{datelimite}\" and \"{date}\" " +
+                $"group by recette.Nom_Recette " +
+                $"order by sum(Quantite_Recette) desc " +
+                $"limit 5;";
+            List<List<string>> Liste_Recette_Semaine = Commandes_SQL.Select_Requete(query);
+
+            for (int i = 0; i < Liste_Recette_Semaine.Count; i++)
+            {
+                ListeView_Recette_Semaine.Items.Add(new Recette_Semaine
+                {
+                    Nom = Liste_Recette_Semaine[i][0],
+                    Type = Liste_Recette_Semaine[i][1],
+                    Createur = Liste_Recette_Semaine[i][2],
+                    Volume_Achete_Semaine = Liste_Recette_Semaine[i][3],
+                    Compteur = Liste_Recette_Semaine[i][4]
+                });
+            }
         }
 
         private void Semaine_Click(object sender, RoutedEventArgs e)
         {
-
+            
         }
 
         public class Recette_Or
         {
             public string Nom { get; set; }
             public string Type { get; set; }
+            public string Compteur { get; set; }
+        }
+
+        public class Recette_Semaine
+        {
+            public string Nom { get; set; }
+            public string Type { get; set; }
+            public string Createur { get; set; }
+            public string Volume_Achete_Semaine { get; set; }
             public string Compteur { get; set; }
         }
     }
